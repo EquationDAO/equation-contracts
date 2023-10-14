acd4bb3155c8437372bbeb1083561922e997210f
Christopher-Michael-Pratt <133328142+Christopher-Michael-Pratt@users.noreply.github.com>
2023-10-14 13:57:39 +0800

    `CVF-49`: replace `^0.8.21` with `^0.8.0` (#49)

--- contracts/tokens/ERC721WeightedVotes.sol
+++ contracts/tokens/ERC721WeightedVotes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/governance/utils/Votes.sol";
