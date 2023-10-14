9ec85507bccddc5360819e0d2a3a71b5d508a39f
Christopher-Michael-Pratt <Christopher.Michael.Pratt@proton.me>
2023-10-14 14:05:06 +0800

    `CVF-66, CVF-67`: add param to `error` type; emit `event` when plugin is registered

--- contracts/plugins/PluginManager.sol
+++ contracts/plugins/PluginManager.sol
@@ -12,16 +12,18 @@ abstract contract PluginManager is IPluginManager, Governable {
 
     /// @inheritdoc IPluginManager
     function registerPlugin(address _plugin) external override onlyGov {
-        if (registeredPlugins[_plugin]) revert PluginAlreadyRegistered();
+        if (registeredPlugins[_plugin]) revert PluginAlreadyRegistered(_plugin);
 
         registeredPlugins[_plugin] = true;
+
+        emit PluginRegistered(_plugin);
     }
 
     /// @inheritdoc IPluginManager
     function approvePlugin(address _plugin) external override {
-        if (pluginApprovals[msg.sender][_plugin]) revert PluginAlreadyApproved();
+        if (pluginApprovals[msg.sender][_plugin]) revert PluginAlreadyApproved(msg.sender, _plugin);
 
-        if (!registeredPlugins[_plugin]) revert PluginNotRegistered();
+        if (!registeredPlugins[_plugin]) revert PluginNotRegistered(_plugin);
 
         pluginApprovals[msg.sender][_plugin] = true;
         emit PluginApproved(msg.sender, _plugin);
@@ -29,7 +31,7 @@ abstract contract PluginManager is IPluginManager, Governable {
 
     /// @inheritdoc IPluginManager
     function revokePlugin(address _plugin) external {
-        if (!pluginApprovals[msg.sender][_plugin]) revert PluginNotApproved();
+        if (!pluginApprovals[msg.sender][_plugin]) revert PluginNotApproved(msg.sender, _plugin);
 
         delete pluginApprovals[msg.sender][_plugin];
         emit PluginRevoked(msg.sender, _plugin);
@@ -42,9 +44,11 @@ abstract contract PluginManager is IPluginManager, Governable {
 
     /// @inheritdoc IPluginManager
     function registerLiquidator(address _liquidator) external override onlyGov {
-        if (registeredLiquidators[_liquidator]) revert LiquidatorAlreadyRegistered();
+        if (registeredLiquidators[_liquidator]) revert LiquidatorAlreadyRegistered(_liquidator);
 
         registeredLiquidators[_liquidator] = true;
+
+        emit LiquidatorRegistered(_liquidator);
     }
 
     /// @inheritdoc IPluginManager
