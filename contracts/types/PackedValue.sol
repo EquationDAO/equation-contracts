cfb753b06c3780262216cf5599751c1d3cf73aab
Christopher-Michael-Pratt <Christopher.Michael.Pratt@proton.me>
2023-11-14 11:33:06 +0800

    add packed value

---
+++ contracts/types/PackedValue.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+type PackedValue is uint256;
+
+using {
+    packUint16,
+    unpackUint16,
+    packUint24,
+    unpackUint24,
+    packUint32,
+    unpackUint32,
+    packUint216,
+    unpackUint216,
+    packUint232,
+    unpackUint232
+} for PackedValue global;
+
+function packUint16(PackedValue self, uint16 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint16(PackedValue self, uint8 position) pure returns (uint16) {
+    return uint16((PackedValue.unwrap(self) >> position) & 0xffff);
+}
+
+function packUint24(PackedValue self, uint24 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint24(PackedValue self, uint8 position) pure returns (uint24) {
+    return uint24((PackedValue.unwrap(self) >> position) & 0xffffff);
+}
+
+function packUint32(PackedValue self, uint32 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint32(PackedValue self, uint8 position) pure returns (uint32) {
+    return uint32((PackedValue.unwrap(self) >> position) & 0xffffffff);
+}
+
+function packUint216(PackedValue self, uint216 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint216(PackedValue self, uint8 position) pure returns (uint216) {
+    return uint216((PackedValue.unwrap(self) >> position) & 0xffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+}
+
+function packUint232(PackedValue self, uint232 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint232(PackedValue self, uint8 position) pure returns (uint232) {
+    return
+        uint232((PackedValue.unwrap(self) >> position) & 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+}
