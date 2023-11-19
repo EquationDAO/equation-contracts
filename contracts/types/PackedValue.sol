7f4e480bab64b4b0cf3aced426cc6cbdb0725563
Christopher-Michael-Pratt <Christopher.Michael.Pratt@proton.me>
2023-11-19 17:09:24 +0800

    add mixed executor v2 to save gas

--- contracts/types/PackedValue.sol
+++ contracts/types/PackedValue.sol
@@ -4,19 +4,37 @@ pragma solidity ^0.8.0;
 type PackedValue is uint256;
 
 using {
+    packUint8,
+    unpackUint8,
     packUint16,
     unpackUint16,
     packUint24,
     unpackUint24,
     packUint32,
     unpackUint32,
+    packUint96,
+    unpackUint96,
+    packUint160,
+    unpackUint160,
     packUint200,
     unpackUint200,
     packUint216,
     unpackUint216,
     packUint232,
-    unpackUint232
+    unpackUint232,
+    packAddress,
+    unpackAddress,
+    packBool,
+    unpackBool
 } for PackedValue global;
+
+function packUint8(PackedValue self, uint8 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint8(PackedValue self, uint8 position) pure returns (uint8) {
+    return uint8((PackedValue.unwrap(self) >> position) & 0xff);
+}
 
 function packUint16(PackedValue self, uint16 value, uint8 position) pure returns (PackedValue) {
     return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
@@ -42,6 +60,22 @@ function unpackUint32(PackedValue self, uint8 position) pure returns (uint32) {
     return uint32((PackedValue.unwrap(self) >> position) & 0xffffffff);
 }
 
+function packUint96(PackedValue self, uint96 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint96(PackedValue self, uint8 position) pure returns (uint96) {
+    return uint96((PackedValue.unwrap(self) >> position) & 0xffffffffffffffffffffffff);
+}
+
+function packUint160(PackedValue self, uint160 value, uint8 position) pure returns (PackedValue) {
+    return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
+}
+
+function unpackUint160(PackedValue self, uint8 position) pure returns (uint160) {
+    return uint160((PackedValue.unwrap(self) >> position) & 0x00ffffffffffffffffffffffffffffffffffffffff);
+}
+
 function packUint200(PackedValue self, uint200 value, uint8 position) pure returns (PackedValue) {
     return PackedValue.wrap(PackedValue.unwrap(self) | (uint256(value) << position));
 }
@@ -65,4 +99,20 @@ function packUint232(PackedValue self, uint232 value, uint8 position) pure retur
 function unpackUint232(PackedValue self, uint8 position) pure returns (uint232) {
     return
         uint232((PackedValue.unwrap(self) >> position) & 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+}
+
+function packBool(PackedValue self, bool value, uint8 position) pure returns (PackedValue) {
+    return packUint8(self, value ? 1 : 0, position);
+}
+
+function unpackBool(PackedValue self, uint8 position) pure returns (bool) {
+    return ((PackedValue.unwrap(self) >> position) & 0x1) == 1;
+}
+
+function packAddress(PackedValue self, address value, uint8 position) pure returns (PackedValue) {
+    return packUint160(self, uint160(value), position);
+}
+
+function unpackAddress(PackedValue self, uint8 position) pure returns (address) {
+    return address(unpackUint160(self, position));
 }
