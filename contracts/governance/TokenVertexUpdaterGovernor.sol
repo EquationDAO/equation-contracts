d6afdff45a99c795c2ddc8e00e979196a28a787c
whyareulaughing <whyareulaughing01@proton.me>
2023-11-27 09:59:11 +0800

    updateTokenVertexConfig: tokenPriceConfig.maxPriceImpactLiquidity = pool.globalLiquidity.liquidity; add maxUpdateTimeDeviation config

--- contracts/governance/TokenVertexUpdaterGovernor.sol
+++ contracts/governance/TokenVertexUpdaterGovernor.sol
@@ -8,10 +8,15 @@ import "../core/interfaces/IConfigurable.sol";
 import "../libraries/Constants.sol";
 
 contract TokenVertexUpdaterGovernor is AccessControl {
+    /// @notice Config data is out-of-date
+    /// @param timestamp Update timestamp
+    error StaleConfig(uint32 timestamp);
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant TOKEN_VERTEX_UPDATER_ROLE = keccak256("TOKEN_VERTEX_UPDATER_ROLE");
 
     IPoolFactory public immutable poolFactory;
+    uint32 public maxUpdateTimeDeviation = 60;
 
     constructor(address _admin, address _tokenVertexUpdater, IPoolFactory _poolFactory) {
         _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
@@ -21,17 +26,24 @@ contract TokenVertexUpdaterGovernor is AccessControl {
         poolFactory = _poolFactory;
     }
 
-    function execute(address _target, uint256 _value, bytes calldata _data) public onlyRole(ADMIN_ROLE) {
-        (bool success, ) = _target.call{value: _value}(_data);
-        require(success, "transaction reverted");
+    function execute(address _target, uint256 _value, bytes calldata _data) public virtual onlyRole(ADMIN_ROLE) {
+        Address.functionCallWithValue(_target, _data, _value);
+    }
+
+    function setMaxUpdateTimeDeviation(uint32 _maxUpdateTimeDeviation) public virtual onlyRole(ADMIN_ROLE) {
+        maxUpdateTimeDeviation = _maxUpdateTimeDeviation;
     }
 
     function updateTokenVertexConfig(
-        IERC20 _token,
-        uint128 _maxPriceImpactLiquidity,
-        PackedValue _packedBalanceRate,
-        PackedValue _packedPremiumRate
-    ) public onlyRole(TOKEN_VERTEX_UPDATER_ROLE) {
+        PackedValue _packedTokenTimestamp,
+        PackedValue _packedBalanceRates,
+        PackedValue _packedPremiumRates
+    ) public virtual onlyRole(TOKEN_VERTEX_UPDATER_ROLE) {
+        IERC20 token = IERC20(_packedTokenTimestamp.unpackAddress(0));
+        uint32 timestamp = _packedTokenTimestamp.unpackUint32(160);
+        uint32 blockTimestamp = uint32(block.timestamp);
+        uint32 timeDelta = timestamp > blockTimestamp ? timestamp - blockTimestamp : blockTimestamp - timestamp;
+        if (timeDelta > maxUpdateTimeDeviation) revert StaleConfig(timestamp);
         IPoolFactory.TokenConfig memory tokenConfig;
         IPoolFactory.TokenFeeRateConfig memory tokenFeeRateConfig;
         IPoolFactory.TokenPriceConfig memory tokenPriceConfig;
@@ -46,7 +58,7 @@ contract TokenVertexUpdaterGovernor is AccessControl {
             tokenConfig.liquidationExecutionFee,
             tokenConfig.interestRate,
             tokenConfig.maxFundingRate
-        ) = poolFactory.tokenConfigs(_token);
+        ) = poolFactory.tokenConfigs(token);
 
         (
             tokenFeeRateConfig.tradingFeeRate,
@@ -55,19 +67,23 @@ contract TokenVertexUpdaterGovernor is AccessControl {
             tokenFeeRateConfig.referralReturnFeeRate,
             tokenFeeRateConfig.referralParentReturnFeeRate,
             tokenFeeRateConfig.referralDiscountRate
-        ) = poolFactory.tokenFeeRateConfigs(_token);
+        ) = poolFactory.tokenFeeRateConfigs(token);
 
         IConfigurable.VertexConfig[] memory vertices = new IConfigurable.VertexConfig[](Constants.VERTEX_NUM);
         unchecked {
             for (uint8 i; i < Constants.VERTEX_NUM; ++i) {
-                vertices[i].balanceRate = _packedBalanceRate.unpackUint32(i * 32);
-                vertices[i].premiumRate = _packedPremiumRate.unpackUint32(i * 32);
+                vertices[i].balanceRate = _packedBalanceRates.unpackUint32(i * 32);
+                vertices[i].premiumRate = _packedPremiumRates.unpackUint32(i * 32);
             }
         }
 
-        (, tokenPriceConfig.liquidationVertexIndex) = poolFactory.tokenPriceConfigs(_token);
-        tokenPriceConfig.maxPriceImpactLiquidity = _maxPriceImpactLiquidity;
+        (tokenPriceConfig.maxPriceImpactLiquidity, tokenPriceConfig.liquidationVertexIndex) = poolFactory
+            .tokenPriceConfigs(token);
+        IPool pool = poolFactory.pools(token);
+        if (address(pool) != address(0)) {
+            (, , , , tokenPriceConfig.maxPriceImpactLiquidity, ) = pool.globalLiquidityPosition();
+        }
         tokenPriceConfig.vertices = vertices;
-        poolFactory.updateTokenConfig(_token, tokenConfig, tokenFeeRateConfig, tokenPriceConfig);
+        poolFactory.updateTokenConfig(token, tokenConfig, tokenFeeRateConfig, tokenPriceConfig);
     }
 }
--- test/governance/TokenVertexUpdaterGovernor.test.ts
+++ test/governance/TokenVertexUpdaterGovernor.test.ts
@@ -2,13 +2,15 @@ import {ethers} from "hardhat";
 import {expect} from "chai";
 import {concatPoolCreationCode} from "../shared/creationCode";
 import {newTokenConfig, newTokenFeeRateConfig, newTokenPriceConfig} from "../shared/tokenConfig";
-import {initializePoolByteCode} from "../shared/address";
+import {computePoolAddress, initializePoolByteCode} from "../shared/address";
+import {time} from "@nomicfoundation/hardhat-network-helpers";
+import {ERC20Test} from "../../typechain-types";
+import {DECIMALS_18, DECIMALS_6, toPriceX96} from "../shared/Constants";
+import {BigNumber} from "ethers";
 
 describe("TokenVertexUpdaterGovernor", () => {
     async function deployFixture() {
         const [s0, s1, s2] = await ethers.getSigners();
-
-        const zeroAddress = "0x0000000000000000000000000000000000000000";
 
         const PoolUtil = await ethers.getContractFactory("PoolUtil");
         const poolUtil = await PoolUtil.deploy();
@@ -38,18 +40,48 @@ describe("TokenVertexUpdaterGovernor", () => {
             liquidityPositionUtil.address
         );
 
+        const ERC20 = await ethers.getContractFactory("ERC20Test");
+        const USDC = (await ERC20.deploy("USDC", "USDC", 6, 100_000_000n * 10n ** 18n)) as ERC20Test;
+        await USDC.deployed();
+        const ETH = (await ERC20.deploy("ETH", "ETH", 18, 100_000_000n * 10n ** 18n)) as ERC20Test;
+        await ETH.deployed();
+
+        const FeeDistributor = await ethers.getContractFactory("MockFeeDistributor");
+        const feeDistributor = await FeeDistributor.deploy();
+        await feeDistributor.deployed();
+
+        const MockPriceFeed = await ethers.getContractFactory("MockPriceFeed");
+        const mockPriceFeed = await MockPriceFeed.deploy();
+        await mockPriceFeed.deployed();
+        await mockPriceFeed.setMinPriceX96(ETH.address, toPriceX96("1808.234", DECIMALS_18, DECIMALS_6));
+        await mockPriceFeed.setMaxPriceX96(ETH.address, toPriceX96("1808.235", DECIMALS_18, DECIMALS_6));
+
+        const MockRewardFarmCallback = await ethers.getContractFactory("MockRewardFarmCallback");
+        const mockRewardFarmCallback = await MockRewardFarmCallback.deploy();
+        await mockRewardFarmCallback.deployed();
+
+        const MockFeeDistributorCallback = await ethers.getContractFactory("MockFeeDistributorCallback");
+        const mockFeeDistributorCallback = await MockFeeDistributorCallback.deploy();
+        await mockFeeDistributorCallback.deployed();
+
+        const EFC = await ethers.getContractFactory("MockEFC");
+        const efc = await EFC.deploy();
+        await efc.deployed();
+        await efc.initialize(100n, mockRewardFarmCallback.address);
+
         const PoolFactory = await ethers.getContractFactory("PoolFactory");
         const poolFactory = await PoolFactory.deploy(
-            zeroAddress,
-            zeroAddress,
-            zeroAddress,
-            zeroAddress,
-            zeroAddress,
-            zeroAddress
+            USDC.address,
+            efc.address,
+            s0.address,
+            mockPriceFeed.address,
+            feeDistributor.address,
+            mockRewardFarmCallback.address
         );
         await poolFactory.deployed();
         await concatPoolCreationCode(poolFactory);
-        await poolFactory.enableToken(zeroAddress, newTokenConfig(), newTokenFeeRateConfig(), newTokenPriceConfig());
+        await poolFactory.enableToken(ETH.address, newTokenConfig(), newTokenFeeRateConfig(), newTokenPriceConfig());
+        await poolFactory.createPool(ETH.address);
 
         const TokenVertexUpdaterGovernor = await ethers.getContractFactory("TokenVertexUpdaterGovernor");
         const tokenVertexUpdaterGovernor = await TokenVertexUpdaterGovernor.deploy(
@@ -57,6 +89,17 @@ describe("TokenVertexUpdaterGovernor", () => {
             s1.address,
             poolFactory.address
         );
+        const poolAddress = computePoolAddress(poolFactory.address, ETH.address, USDC.address);
+        const Pool = await ethers.getContractFactory("Pool", {
+            libraries: {
+                PoolUtil: poolUtil.address,
+                FundingRateUtil: fundingRateUtil.address,
+                PriceUtil: priceUtil.address,
+                PositionUtil: positionUtil.address,
+                LiquidityPositionUtil: liquidityPositionUtil.address,
+            },
+        });
+        const pool = Pool.attach(poolAddress);
 
         await poolFactory.changeGov(tokenVertexUpdaterGovernor.address);
         await tokenVertexUpdaterGovernor.execute(
@@ -65,11 +108,11 @@ describe("TokenVertexUpdaterGovernor", () => {
             PoolFactory.interface.encodeFunctionData("acceptGov", [])
         );
 
-        return {s0, s1, s2, zeroAddress, tokenVertexUpdaterGovernor, poolFactory};
+        return {s0, s1, s2, tokenVertexUpdaterGovernor, poolFactory, pool, ETH, USDC};
     }
 
     it("updateTokenVertexConfig test", async () => {
-        const {s0, s1, s2, zeroAddress, tokenVertexUpdaterGovernor, poolFactory} = await deployFixture();
+        const {s0, s1, s2, tokenVertexUpdaterGovernor, poolFactory, pool, ETH, USDC} = await deployFixture();
         const balanceRateList = [0n, 100000n, 2000000n, 3000000n, 4000000n, 5000000n, 6000000n]; // 0->1%->2%...6%
         const premiumRateList = [0n, 60000n, 200000n, 250000n, 300000n, 700000n, 11000000n]; // [0%. 0.06%, 0.2%, 0.25%, 0.3%, 0.7%, 11%]
         let packedBalanceRate = 0n;
@@ -79,32 +122,47 @@ describe("TokenVertexUpdaterGovernor", () => {
             packedPremiumRate += premiumRateList[i] << (32n * BigInt(i));
         }
         const regexp: RegExp = /is missing role 0x0833596c19f8afe7d32bf2c778d80a2e2b0dcaa54c9ed6c8df6d646a481f4f89$/;
+        const now = await time.latest();
+        const packedTokenTimestamp = BigNumber.from(ETH.address).toBigInt() + BigInt(now) * 2n ** 160n;
         await expect(
             tokenVertexUpdaterGovernor
                 .connect(s0)
-                .updateTokenVertexConfig(zeroAddress, 100000000000000n, packedBalanceRate, packedPremiumRate)
+                .updateTokenVertexConfig(packedTokenTimestamp, packedBalanceRate, packedPremiumRate)
         ).to.be.revertedWith(regexp);
         await expect(
             tokenVertexUpdaterGovernor
                 .connect(s2)
-                .updateTokenVertexConfig(zeroAddress, 100000000000000n, packedBalanceRate, packedPremiumRate)
+                .updateTokenVertexConfig(packedTokenTimestamp, packedBalanceRate, packedPremiumRate)
         ).to.be.revertedWith(regexp);
 
-        const beforeTokenConfigs = await poolFactory.tokenConfigs(zeroAddress);
-        const beforeTokenFeeRateConfigs = await poolFactory.tokenFeeRateConfigs(zeroAddress);
+        const beforeTokenConfigs = await poolFactory.tokenConfigs(ETH.address);
+        const beforeTokenFeeRateConfigs = await poolFactory.tokenFeeRateConfigs(ETH.address);
+        await USDC.transfer(pool.address, beforeTokenConfigs.minMarginPerPosition);
+        await pool.openLiquidityPosition(
+            s0.address,
+            beforeTokenConfigs.minMarginPerPosition,
+            beforeTokenConfigs.minMarginPerPosition.mul(200n)
+        );
+
+        const stalePackedTokenTimestamp = BigNumber.from(ETH.address).toBigInt() + BigInt(now - 61) * 2n ** 160n;
+        await expect(
+            tokenVertexUpdaterGovernor
+                .connect(s1)
+                .updateTokenVertexConfig(stalePackedTokenTimestamp, packedBalanceRate, packedPremiumRate)
+        ).to.be.revertedWithCustomError(tokenVertexUpdaterGovernor, "StaleConfig");
 
         await tokenVertexUpdaterGovernor
             .connect(s1)
-            .updateTokenVertexConfig(zeroAddress, 100000000000000n, packedBalanceRate, packedPremiumRate);
+            .updateTokenVertexConfig(packedTokenTimestamp, packedBalanceRate, packedPremiumRate);
         for (let i = 0; i < balanceRateList.length; i++) {
-            const {balanceRate, premiumRate} = await poolFactory.tokenPriceVertexConfigs(zeroAddress, i);
+            const {balanceRate, premiumRate} = await poolFactory.tokenPriceVertexConfigs(ETH.address, i);
             expect(balanceRate).eq(balanceRateList[i]);
             expect(premiumRate).eq(premiumRateList[i]);
         }
-        const {maxPriceImpactLiquidity} = await poolFactory.tokenPriceConfigs(zeroAddress);
-        expect(maxPriceImpactLiquidity).eq(100000000000000n);
-        const afterTokenConfigs = await poolFactory.tokenConfigs(zeroAddress);
-        const afterTokenFeeRateConfigs = await poolFactory.tokenFeeRateConfigs(zeroAddress);
+        const {maxPriceImpactLiquidity} = await poolFactory.tokenPriceConfigs(ETH.address);
+        expect(maxPriceImpactLiquidity).eq(beforeTokenConfigs.minMarginPerPosition.mul(200n));
+        const afterTokenConfigs = await poolFactory.tokenConfigs(ETH.address);
+        const afterTokenFeeRateConfigs = await poolFactory.tokenFeeRateConfigs(ETH.address);
         expect(JSON.stringify(beforeTokenConfigs)).equals(JSON.stringify(afterTokenConfigs));
         expect(JSON.stringify(beforeTokenFeeRateConfigs)).equals(JSON.stringify(afterTokenFeeRateConfigs));
     });
