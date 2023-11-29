3fe3c7cea31e6655aa7365f0272508a81c674f0f
whyareulaughing <132743029+whyareulaughing@users.noreply.github.com>
2023-11-29 15:55:28 +0800

    split updateTokenVertexConfig (#94)
    * split updateTokenVertexConfig into updateTokenVertexConfigPremiumRates and updateTokenVertexConfigBalanceRates
    
    * add key word `unchecked`, `virtual`

--- contracts/governance/TokenVertexUpdaterGovernor.sol
+++ contracts/governance/TokenVertexUpdaterGovernor.sol
@@ -2,13 +2,14 @@
 pragma solidity =0.8.21;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
 import "../core/interfaces/IPoolFactory.sol";
 import "../types/PackedValue.sol";
 import "../core/interfaces/IConfigurable.sol";
 import "../libraries/Constants.sol";
 import "../libraries/SafeCast.sol";
 
-contract TokenVertexUpdaterGovernor is AccessControl {
+contract TokenVertexUpdaterGovernor is AccessControl, Multicall {
     using SafeCast for uint256;
     /// @notice Config data is out-of-date
     /// @param timestamp Update timestamp
@@ -36,11 +37,25 @@ contract TokenVertexUpdaterGovernor is AccessControl {
         maxUpdateTimeDeviation = _maxUpdateTimeDeviation;
     }
 
-    function updateTokenVertexConfig(
+    function updateTokenVertexConfigPremiumRates(
         PackedValue _packedTokenTimestamp,
-        PackedValue _packedBalanceRates,
         PackedValue _packedPremiumRates
     ) public virtual onlyRole(TOKEN_VERTEX_UPDATER_ROLE) {
+        _updateTokenVertexConfig(_packedTokenTimestamp, _packedPremiumRates, _verticesUpdatePremiumRates);
+    }
+
+    function updateTokenVertexConfigBalanceRates(
+        PackedValue _packedTokenTimestamp,
+        PackedValue _packedBalanceRates
+    ) public virtual onlyRole(TOKEN_VERTEX_UPDATER_ROLE) {
+        return _updateTokenVertexConfig(_packedTokenTimestamp, _packedBalanceRates, _verticesUpdateBalanceRates);
+    }
+
+    function _updateTokenVertexConfig(
+        PackedValue _packedTokenTimestamp,
+        PackedValue _packedData,
+        function(IERC20, PackedValue) returns (IConfigurable.VertexConfig[] memory) _verticesFn
+    ) internal virtual {
         unchecked {
             IERC20 token = IERC20(_packedTokenTimestamp.unpackAddress(0));
             uint64 timestamp = _packedTokenTimestamp.unpackUint64(160);
@@ -72,19 +87,43 @@ contract TokenVertexUpdaterGovernor is AccessControl {
                 tokenFeeRateConfig.referralDiscountRate
             ) = poolFactory.tokenFeeRateConfigs(token);
 
-            IConfigurable.VertexConfig[] memory vertices = new IConfigurable.VertexConfig[](Constants.VERTEX_NUM);
-            for (uint8 i; i < Constants.VERTEX_NUM; ++i) {
-                vertices[i].balanceRate = _packedBalanceRates.unpackUint32(i * 32);
-                vertices[i].premiumRate = _packedPremiumRates.unpackUint32(i * 32);
-            }
-
             IPoolFactory.TokenPriceConfig memory tokenPriceConfig;
             (, tokenPriceConfig.liquidationVertexIndex) = poolFactory.tokenPriceConfigs(token);
             IPool pool = poolFactory.pools(token);
             (, , , , tokenPriceConfig.maxPriceImpactLiquidity, ) = pool.globalLiquidityPosition();
+
+            IConfigurable.VertexConfig[] memory vertices = _verticesFn(token, _packedData);
             tokenPriceConfig.vertices = vertices;
 
             poolFactory.updateTokenConfig(token, tokenConfig, tokenFeeRateConfig, tokenPriceConfig);
+        }
+    }
+
+    function _verticesUpdateBalanceRates(
+        IERC20 _token,
+        PackedValue _packedBalanceRates
+    ) internal view virtual returns (IConfigurable.VertexConfig[] memory) {
+        unchecked {
+            IConfigurable.VertexConfig[] memory vertices = new IConfigurable.VertexConfig[](Constants.VERTEX_NUM);
+            for (uint8 i; i < Constants.VERTEX_NUM; ++i) {
+                vertices[i].balanceRate = _packedBalanceRates.unpackUint32(i * 32);
+                (, vertices[i].premiumRate) = poolFactory.tokenPriceVertexConfigs(_token, i);
+            }
+            return vertices;
+        }
+    }
+
+    function _verticesUpdatePremiumRates(
+        IERC20 _token,
+        PackedValue _packedPremiumRates
+    ) internal view virtual returns (IConfigurable.VertexConfig[] memory) {
+        unchecked {
+            IConfigurable.VertexConfig[] memory vertices = new IConfigurable.VertexConfig[](Constants.VERTEX_NUM);
+            for (uint8 i; i < Constants.VERTEX_NUM; ++i) {
+                (vertices[i].balanceRate, ) = poolFactory.tokenPriceVertexConfigs(_token, i);
+                vertices[i].premiumRate = _packedPremiumRates.unpackUint32(i * 32);
+            }
+            return vertices;
         }
     }
 }
